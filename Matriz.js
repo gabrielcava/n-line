0cb573dccef26084ad15c6b6f214d68cd6f9adf0
Gabriel Cava <gabrielalejandrocava@gmail.com>
2013-06-08 20:47:35 -0300

    GANASTE!
    Ya tiene logica, para saber cuando se gana la partida.

--- Matriz.js
+++ Matriz.js
@@ -1,12 +1,71 @@
 function Matriz(ancho, largo){
-	this.miMatriz = new Array(ancho);
 
-	for (var i = 0; i < ancho; i++){
-		this.miMatriz[i] = new Array(largo);
-	};
+	this.ancho = ancho;
+	this.largo = largo;
+
+	this.miMatriz = new Array(this.ancho);
+
+	for (i = 0; i < this.ancho; i++){
+  		this.miMatriz[i] = new Array(this.largo);
+ 	};
 
 	this.insertarValor = function(posicionX, posicionY, valor){
+		/*Forma chota pero se entiende
+ 		if (!this.miMatriz[posicionX]){
+			this.miMatriz[posicionX] = [];
+		};*/
 		this.miMatriz[posicionX][posicionY]=valor;
+
+		// Forma no tan piola como habiamos pensado, pero la dijo pablo, que es medio pelotudo... pija
+		/*this.miMatriz[posicionX] = this.miMatriz[posicionX] || [];
+		this.miMatriz[posicionX][posicionY] = valor;*/
+		
+		//this.validarGanador(posicionX, posicionY, valor);
 	};
+
+	this.validarPosicion = function(posicionX, posicionY){
+		return !((this.miMatriz[posicionX][posicionY] == "X") || (this.miMatriz[posicionX][posicionY] == "O"))
+	};
+
+	this.validarGanador = function(posicionX, posicionY, valor){
+		var direcciones = [];
+		var contadorDeValores = [];
+		var ganador = false;
+
+		for (var i = 1; i <= 8; i++) {
+			direcciones[i] = true;
+		};
+
+		for (var i = 1; i <= 4; i++) {
+			contadorDeValores[i] = 1;
+		}
+
+		for (var i = 1; i <= 5; i++) {
+			for (var j = 1; j <= 8; j++) {
+				if (direcciones[j]){
+					var celdaConjuntaX = i*Math.round(Math.cos(Math.PI*j/4));
+					var celdaConjuntaY = i*Math.round(Math.sin(Math.PI*j/4));
+					direcciones[j] = (this.miMatriz[(posicionX+celdaConjuntaX)][(posicionY+celdaConjuntaY)] == valor);
+					if (direcciones[j] && j <= 4){
+						contadorDeValores[j]++;
+						ganador = ganador || (contadorDeValores[j] >= 5);
+					}
+					else if (direcciones[j]){
+						contadorDeValores[j-4]++;
+						ganador = ganador || (contadorDeValores[j-4] >= 5);
+					};
+					/*(function(posicionX2, posicionY2, x2, y2){
+						setTimeout(function(){
+							contexto.fillStyle = 'black';
+							contexto.fillRect((posicionX2+x2)*50, (posicionY2+y2)*50, 50, 50);
+						}, 200*j*i);
+					})(posicionX, posicionY, celdaConjuntaX, celdaConjuntaY)*/
+				};
+			};
+		};
+
+		return ganador;
+
+	}
 
 }
--- Script.js
+++ Script.js
@@ -1,52 +1,98 @@
 $(document).ready(function(){
 
 		var canvas = $(document.getElementById('grilla'));
-
 		var contexto = canvas[0].getContext('2d');
-
+		
+		//var contexto = window.contexto = canvas[0].getContext('2d');
+		
 		var miMatriz = new Matriz(12, 12);
+		var turno = true;
 
-		var a = true;
+		generarGrilla(contexto, miMatriz.ancho, miMatriz.largo);
 
-		for (var i = 0; i < 12; i++) {
-			for (var j = 0; j < 12; j++) {
-				contexto.lineWidth = 1;
-				contexto.strokeStyle = 'black';
-				contexto.strokeRect(j*50, i*50, 50, 50);
-			};
-		};
 
 		canvas.click(function(e){
-			contexto.lineWidth = 5;
-			contexto.strokeStyle = 'black';
+			contexto.clearRect(0, 0, 600, 600);
 
 			var posicionX = parseInt(e.offsetX/50);
-
 			var posicionY = parseInt(e.offsetY/50);
 
-			miMatriz.insertarValor(posicionX, posicionY, a);
+			if (miMatriz.validarPosicion(posicionX, posicionY)){
+				insertarValorEnMatriz(miMatriz, posicionX, posicionY, turno);
+				if (validarGanadorEnMatriz(miMatriz, posicionX, posicionY, turno)){
+					setTimeout(function(){alert("GANASTE!");}, 1000);
+				};
+				turno = !turno;
+			};
+			generarGrilla(contexto, miMatriz.ancho, miMatriz.largo);
+			refrescarGrilla(contexto, miMatriz);
+		});
 
-			if (a) {
-				contexto.moveTo(posicionX*50, posicionY*50);
-				contexto.lineTo(50+(posicionX*50), 50+(posicionY*50));
-
-				contexto.moveTo((posicionX*50)+50, posicionY*50);
-				contexto.lineTo(posicionX*50, 50+(posicionY*50));
-				a = false;
-			}
+});
 
 
-			else {
-				contexto.beginPath();
-    			contexto.arc(25+(posicionX*50),25+(posicionY*50), 20, 0, 360, false);
-    			a = true;
-			}
+function refrescarGrilla(contexto, miMatriz){
+
+	contexto.lineWidth = 5;
+	contexto.strokeStyle = 'black';
 			
+	for (var i = 0; i < miMatriz.ancho; i++) {
+		for (var j = 0; j < miMatriz.largo; j++) {
 
-			contexto.stroke();
+			var posX = i*50
+			var posY = j*50
 
-
+			if (miMatriz.miMatriz[i][j] == "X"){
+				DibujarCruz(contexto, posX+10, posY+10, 30);
 			}
-		);
+			else if (miMatriz.miMatriz[i][j] == "O"){
+				DibujarCirculo(contexto, posX+25, posY+25, 15);
+			}
+		};
+	};
+};
 
-	});
+function generarGrilla(contexto, ancho, largo){
+	for (var i = 0; i < ancho; i++) {
+		for (var j = 0; j < largo; j++) {
+			contexto.lineWidth = 1;
+			contexto.strokeStyle = 'black';
+			contexto.strokeRect(j*50, i*50, 50, 50);
+		};
+	};
+};
+
+function insertarValorEnMatriz(miMatriz, posicionX, posicionY, turno){
+	if (turno){
+		miMatriz.insertarValor(posicionX, posicionY, "X");
+	}
+	else{
+		miMatriz.insertarValor(posicionX, posicionY, "O");
+	};
+};
+
+function DibujarCruz(context, posX, posY, tam){
+ 	context.beginPath();
+ 	context.moveTo(posX, posY);
+ 	context.lineTo(posX + tam, posY + tam);
+ 	context.moveTo(posX + tam, posY);
+ 	context.lineTo(posX , posY + tam);
+ 	context.closePath();
+    context.stroke();
+};
+
+function DibujarCirculo(context, posX, posY, radio){
+ 	context.beginPath();
+ 	context.arc(posX,posY, radio, 0, 360, false); 
+ 	context.closePath(); 
+ 	context.stroke();
+};
+
+function validarGanadorEnMatriz(miMatriz, posicionX, posicionY, turno){
+	if (turno){
+		return (miMatriz.validarGanador(posicionX, posicionY, "X"));
+	}
+	else{
+		return (miMatriz.validarGanador(posicionX, posicionY, "O"));
+	};
+};
